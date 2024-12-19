14855b76b73d02010df22399b3c1b3ecf2c856f5
Nishan Bista <nishanbista525@gmail.com>
2024-12-19 10:15:07 +0545

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 
 function App() {
+    const [yes, setyes] = useState(second);
 
-  return (
-    <>
-      <div>
-        <h1 className="bg-red-500">Hello world</h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <h1 className="bg-red-500">Hello world</h1>
+            </div>
+        </>
+    );
 }
 
 export default App;
