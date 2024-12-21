0f03cf820ea83a92d66b292b7c67da4b5190f474
Nishan Bista <nishanbista525@gmail.com>
2024-12-21 17:00:12 +0545

    feat: Navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 function NavBar() {
     return (
         <>
-            <div className="bg-green-400 h-[200px]">NavBar</div>
+            <div className="h-[60px] bg-gray-300 flex items-center justify-evenly">
+                <NavLink
+                    to={"/home"}
+                    className={({ isActive }) =>
+                        isActive ? "text-green-500" : "text-black"
+                    }
+                >
+                    Home
+                </NavLink>
+
+                <NavLink
+                    to={"/signup"}
+                    className={({ isActive }) =>
+                        isActive ? "text-green-500" : "text-black"
+                    }
+                >
+                    Signup
+                </NavLink>
+                <NavLink
+                    to={"/login"}
+                    className={({ isActive }) =>
+                        isActive ? "text-yellow-500" : "text-black"
+                    }
+                >
+                    Login
+                </NavLink>
+            </div>
         </>
     );
 }
