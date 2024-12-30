2f24a592024b56811d8c402412b37c8d828d0156
Nishan Bista <nishanbista525@gmail.com>
2024-12-30 14:21:34 +0545

    Btton componenet

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer";
 import Loader from "./Loader";
 import Protected from "./Protected";
 import UserCard from "./UserCard";
+import Button from "./Button";
 
-export { NavBar, Footer, Loader, Protected, UserCard };
+export { NavBar, Footer, Loader, Protected, UserCard, Button };
