6a7ce9b456ae6fffa752ac8d17e78a84c669e10f
Manishhh10 <manishbhatt2121714@gmail.com>
2025-01-03 21:45:56 +0545

    minor changes

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,5 +5,6 @@ import Protected from "./Protected";
 import UserCard from "./UserCard";
 import Button from "./Button";
 import EnterEmailOtp from "./EnterEmailOtp";
+import {Dropdown,DropdownItem} from "./DropDown";
 
-export { NavBar, Footer, Loader, Protected, UserCard, Button, EnterEmailOtp };
+export { NavBar, Footer, Loader, Protected, UserCard, Button, EnterEmailOtp, Dropdown, DropdownItem};
