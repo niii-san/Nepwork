166472682519d703b2285a7ff9f29420ebe99f85
Rehan Pradhan <rehanpradhan10@gmail.com>
2025-01-06 08:57:43 +0545

    kyc model error fixed

--- server/src/models/kyc.model.js
+++ server/src/models/kyc.model.js
@@ -74,7 +74,7 @@ const kycSchema = new mongoose.Schema(
             default: "pending",
         },
     },
-    { timestamps: true },
+    { timestamps: true } , 
 );
 
 export const Kyc = mongoose.model("Kyc", kycSchema);
