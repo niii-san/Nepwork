a99b736164f48429a922e864ab8a389178dd67ad
Nishan Bista <nishanbista525@gmail.com>
2025-01-14 14:34:07 +0545

    freelancer only middleware

--- server/src/middlewares/clientOnly.middleware.js
+++ server/src/middlewares/clientOnly.middleware.js
@@ -2,7 +2,7 @@ import { ApiError, asyncHandler } from "../utils/index.js";
 
 export const clientOnly = asyncHandler(async (req, _, next) => {
     if (req.user.role !== "client")
-        throw new ApiError(401, true, "Only client allowed for this method ");
+        throw new ApiError(401, true, "Only client is allowed for this");
 
     next();
 });
--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -1,6 +1,7 @@
 import authenticate from "./authenticate.middleware.js";
 import { authorizeAdmin } from "./authorizeAdmin.middleware.js";
 import { clientOnly } from "./clientOnly.middleware.js";
+import { freelancerOnly } from "./freelancerOnly.middleware.js";
 import { verified } from "./verified.middleware.js";
 
-export { authenticate, authorizeAdmin, verified, clientOnly };
+export { authenticate, authorizeAdmin, verified, clientOnly, freelancerOnly };
