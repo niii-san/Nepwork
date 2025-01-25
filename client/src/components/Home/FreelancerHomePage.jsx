5c102d97cec853b0f207bc5055a9c8f569497785
Nishan Bista <nishanbista525@gmail.com>
2025-01-25 20:40:48 +0545

    feat: render client/freelancer home page conditionally

---
+++ client/src/components/Home/FreelancerHomePage.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import FreelancerCard from '../FreelancerCard'
+import { FaArrowRightLong } from "react-icons/fa6";
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+import JobCard from '../JobCard';
+
+function FreelancerHomePage({isLoggedIn,userData}) {
+  return (
+        <>
+            <div className="bg-secondary text-center ">
+                {/* You're not Logged in */}
+            </div>
+            <div className="mt-12 mb-[800px] flex max-w-full justify-center items-center flex-col">
+                <div className="bg-primary w-[90%] rounded-xl max-w-[1200px]">
+                    <h1 className="text-4xl font-semibold text-center text-whitetext mb-4 mt-12">
+                        Work from anywhere, hire for <br /> anything!
+                    </h1>
+                    <div className="flex justify-center items-center mb-12">
+                        <div className="w-[60%] max-w-[450px]">
+                            <div className="mt-1 flex items-center flex-row w-full rounded-lg bg-white border border-hover_button focus:shadow-md">
+                                <input
+                                    id="search"
+                                    placeholder="Search jobs"
+                                    className="rounded-md p-3 w-full bg-transparent outline-none px-4 text-base "
+                                />
+                                <button className="bg-primary text-whitetext p-3 rounded-lg mr-1">
+                                    <FaArrowRightLong />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="text-whitetext ml-2 text-3xl">
+                            <HiOutlineAdjustmentsHorizontal />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center items-center flex-wrap gap-6">
+                    freelancer dashboard
+                
+                </div>
+            </div>
+        </>
+  )
+}
+
+export default FreelancerHomePage
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,48 +1,26 @@
 import React from "react";
-import { FaArrowRightLong } from "react-icons/fa6";
-import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
-import { FreelancerCard,JobCard } from "../components";
+import { useAuth, useUser } from "../stores";
+import { ClientHomepage, FreelancerHomePage, Loader } from "../components";
 
 function Home() {
+    const userData = useUser((state) => state.data);
+    const isLoggedIn = useAuth((state) => state.isLoggedIn);
+    console.log(userData);
+    console.log(isLoggedIn);
 
-
-        return (
-            <>
-                <div className="bg-secondary text-center ">
-                    {/* You're not Logged in */}
-                </div>
-                <div className="mt-12 mb-[800px] flex max-w-full justify-center items-center flex-col">
-                    <div className="bg-primary w-[90%] rounded-xl max-w-[1200px]">
-                        <h1 className="text-4xl font-semibold text-center text-whitetext mb-4 mt-12">
-                            Work from anywhere, hire for <br /> anything!
-                        </h1>
-                        <div className="flex justify-center items-center mb-12">
-                            <div className="w-[60%] max-w-[450px]">
-                                <div className="mt-1 flex items-center flex-row w-full rounded-lg bg-white border border-hover_button focus:shadow-md">
-                                    <input
-                                        id="search"
-                                        placeholder="Search"
-                                        className="rounded-md p-3 w-full bg-transparent outline-none px-4 text-base "
-                                    />
-                                    <button className="bg-primary text-whitetext p-3 rounded-lg mr-1">
-                                        <FaArrowRightLong />
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="text-whitetext ml-2 text-3xl">
-                                <HiOutlineAdjustmentsHorizontal />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-center items-center flex-wrap gap-6">
-                    <FreelancerCard/>
-                        <FreelancerCard/>
-                        <FreelancerCard/>
-                        <FreelancerCard/>
-                    </div>
-                </div>
-            </>
-        );
+    if (isLoggedIn && !userData) {
+        return <Loader />;
     }
+
+    if (!isLoggedIn || userData?.role === "client") {
+        return <ClientHomepage />;
+    }
+
+    if (isLoggedIn && userData?.role === "freelancer") {
+        return <FreelancerHomePage />;
+    }
+
+    return <div>Something went wrong!</div>;
+}
 
 export default Home;
