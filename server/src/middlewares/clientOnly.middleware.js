9d627ad57ce588b3d91e10bd9a7e2a7f08148e90
Nishan Bista <nishanbista525@gmail.com>
2025-01-26 21:27:22 +0545

    comments update

--- server/src/middlewares/clientOnly.middleware.js
+++ server/src/middlewares/clientOnly.middleware.js
@@ -1,5 +1,7 @@
 import { ApiError, asyncHandler } from "../utils/index.js";
 
+// NOTE: Only use this middleware after authenticate middleware
+// this middleware is also not standalone middleware this just checks if req.user.role is client or not and forward or rejects
 export const clientOnly = asyncHandler(async (req, _, next) => {
     if (req.user.role !== "client")
         throw new ApiError(401, true, "Only client is allowed for this");
