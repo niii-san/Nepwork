c9f64714bc38b00d950808d6096e7573b34528aa
Nishan Bista <nishanbista525@gmail.com>
2025-01-26 12:31:17 +0545

    feat: get homepage jobs for freelancers

---
+++ server/src/controllers/job/getHomePageJobs.controller.js
@@ -0,0 +1,43 @@
+import { ApiResponse, asyncHandler } from "../../utils/index.js";
+import { Job } from "../../models/job.model.js";
+import mongoose from "mongoose";
+
+export const getHomePageJobs = asyncHandler(async (req, res) => {
+    const userId = req.body.userId ?? "";
+
+    // if userId not provided send all jobs
+    if (!userId) {
+        const jobs = await Job.find().populate({
+            path: "postedBy",
+            select: "name avatar _id",
+        });
+        return res
+            .status(200)
+            .json(new ApiResponse(200, true, false, "fetched all jobs", jobs));
+    }
+
+    if (userId && mongoose.isValidObjectId(userId)) {
+        const jobs = await getRecommendedJobs(userId);
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    true,
+                    false,
+                    `Fetched jobs for ${userId}`,
+                    jobs,
+                ),
+            );
+    }
+
+    // TODO: send jobs that would be more relevent if userId is provided
+    async function getRecommendedJobs(userId) {
+        const jobs = await Job.find().populate({
+            path: "postedBy",
+            select: "name avatar _id",
+        });
+        return jobs;
+    }
+});
