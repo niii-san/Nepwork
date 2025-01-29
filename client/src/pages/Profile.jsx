8e80118d3c771a7fe322ac7fc18f84be43c369c1
Manishhh10 <manishbhatt2121714@gmail.com>
2025-01-29 19:25:45 +0545

    ownprofile completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,9 @@ import { Button, ChangeAvatarModal, Loader } from "../components";
 import toast from "react-hot-toast";
 import api from "../utils/api";
 import default_avatar from "../assets/default_avatar.svg";
+import { IoLocationOutline } from "react-icons/io5";
+import { CiStar } from "react-icons/ci";
+import { BiMessageRoundedDots } from "react-icons/bi";
 import Tag from "../components/Tag";
 
 function Profile() {
@@ -55,46 +58,132 @@ function Profile() {
     return (
         <>
             {changeAvatarModal && (
-                <ChangeAvatarModal setModal={setChangeAvatarModal} refetchProfile={fetchSetCurrentProfileData} />
+                <ChangeAvatarModal
+                    setModal={setChangeAvatarModal}
+                    refetchProfile={fetchSetCurrentProfileData}
+                />
             )}
-            <div className="bg-secondary min-h-[800px]">
-                <div>
-                    <img
-                        src={currentProfileData.avatar ?? default_avatar}
-                        alt={`Profile Photo of ${currentProfileData.name.firstName}`}
-                        className="w-20 h-20"
-                    />
-                    {isOwnProfile && (
-                        <Button onClick={() => setChangeAvatarModal(true)}>
-                            Change profile
-                        </Button>
-                    )}
+            <div className="flex mb-4 flex-col border border-black justify-center items-center">
+                <div className="border border-black mt-8 flex w-[1000px]">
+                    <div className="flex border gap border-red-600 gap-2 items-center flex-col">
+                        <img
+                            src={currentProfileData.avatar ?? default_avatar}
+                            alt={`Profile Photo of ${currentProfileData.name.firstName}`}
+                            className="w-[280px] h-[280px] rounded-[35px] shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25)]"
+                        />
+                        <div className="flex justify-evenly text-lg font-medium w-full">
+                            <span>22 followers</span>
+                            <span>22 following</span>
+                        </div>
+
+                        <div
+                            className={
+                                isOwnProfile
+                                    ? "w-full"
+                                    : "w-[90%] flex justify-between"
+                            }
+                        >
+                            {isOwnProfile ? (
+                                <Button
+                                    className={"w-full"}
+                                    onClick={() => setChangeAvatarModal(true)}
+                                >
+                                    Edit Profile
+                                </Button>
+                            ) : (
+                                <>
+                                    <Button>Follow</Button>
+                                    <button className="bg-[#1C98F7] text-xl text-whitetext rounded-[10px] px-4">
+                                        <BiMessageRoundedDots />
+                                    </button>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                    {/* <div>isOwnProfile : {isOwnProfile ? "Yes" : "No"}</div> */}
+                    <div className="flex ml-[26px] border mt-10 w-[80%] border-purple-600 flex-col">
+                        <div className="text-[#292d32] border border-blue-600 flex justify-between items-center  font-semibold text-[40px]">
+                            <div>
+                                {currentProfileData.name.firstName}{" "}
+                                {currentProfileData.name.lastName}
+                                <span className="border border-[#868686] px-2 ml-2 py-[2px] rounded-[15px] text-[#868686] text-[10px] font-medium">
+                                    Joined{" "}
+                                    {getJoinedTime(
+                                        currentProfileData.createdAt,
+                                    )}
+                                </span>
+                            </div>
+                            <div className="flex flex-col items-center mr-2">
+                                <div className="flex items-center">
+                                    <CiStar className="text-primary" />{" "}
+                                    {currentProfileData.rating}
+                                </div>
+                                {!isOwnProfile && (
+                                    <span className="font-semibold text-xl text-[#5c5f65] mt-1">
+                                        Your Rating: 3
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                        <div className="text-[#868686] flex items-center text-sm font-medium">
+                            <IoLocationOutline />
+                            <span>Kathmandu, Nepal</span>
+                        </div>
+                        <div className="text-base font-medium mt-2">
+                            Available to work:{" "}
+                            {currentProfileData.available ? "Yes" : "No"}
+                        </div>
+                        <div className="bg-[#E4E4E4] text-xl font-medium rounded-sm w-[250px] h-[50px] flex px-2 items-center mt-2 group relative justify-between">
+                            <span>Rs.{currentProfileData.hourlyRate} / Hr</span>
+                            {isOwnProfile && (
+                                <span className="text-xs text-gray-500 hover:text-gray-700 cursor-pointer flex items-center gap-1 border border-[#868686] rounded-lg px-2 py-[1px]">
+                                    <u>Edit Price</u>
+                                </span>
+                            )}
+                        </div>
+                        <div>
+                            {/* {currentProfileData.kyc.address.temporary.city.toUpperCase()}
+                        ,{currentProfileData.kyc.address.temporary.state} */}
+                        </div>
+                        <div>
+                            {/* Verified:{" "}
+                            {currentProfileData.kycVerified ? "yes" : "no"} */}
+                        </div>
+                        <div className="mt-2 flex gap-2 flex-wrap">
+                            <Tag title="Design" />
+                            <Tag title="Frontend" />
+                            <Tag title="Figma" />
+                            <Tag title="Graphic" />
+                            <Tag title="Canva" />
+                            <Tag title="Design" />
+                            <Tag title="Frontend" />
+                            <Tag title="Figma" />
+                            {currentProfileData?.tags?.map((item) => (
+                                <Tag key={item} title={item} />
+                            ))}
+                            {isOwnProfile && (
+                                <span className="text-xs text-primary hover:text-gray-700 cursor-pointer flex items-center gap-1 border border-primary rounded-lg px-2 py-[1px]">
+                                    <u>Update Tags</u>
+                                </span>
+                            )}
+                        </div>
+                    </div>
                 </div>
-                <div>isOwnProfile : {isOwnProfile ? "Yes" : "No"}</div>
-                <div>
-                    Name: {currentProfileData.name.firstName}{" "}
-                    {currentProfileData.name.lastName}
+                <div className="w-[1000px] mt-4 border border-black relative group">
+                    <div className="flex justify-between items-center">
+                        <h3 className="text-[#292d32] text-[26px] font-medium flex items-center gap-2">
+                            About
+                            {isOwnProfile && (
+                                <span className="mt-2 text-xs text-gray-500 hover:text-gray-700 cursor-pointer flex items-center gap-1 border border-[#868686] rounded-lg px-2 py-[1px]">
+                                    <u>Update About</u>
+                                </span>
+                            )}
+                        </h3>
+                    </div>
+                    <p className="text-[#787878] text-base font-medium mt-1">
+                        {currentProfileData.about ?? "No description added yet"}
+                    </p>
                 </div>
-                <div>
-                    {currentProfileData.kyc.address.temporary.city.toUpperCase()}
-                    ,{currentProfileData.kyc.address.temporary.state}
-                </div>
-                <div>About: {currentProfileData.about ?? ""}</div>
-                <div>Joined: {getJoinedTime(currentProfileData.createdAt)}</div>
-                <div>
-                    Verified: {currentProfileData.kycVerified ? "yes" : "no"}
-                </div>
-                <div>
-                    Tags:
-                    {currentProfileData?.tags?.map((item) => (
-                        <Tag key={item} title={item} />
-                    ))}
-                </div>
-                <div>
-                    Available: {currentProfileData.available ? "yes" : "no"}
-                </div>
-                <div>Hourly Rate: {currentProfileData.hourlyRate}</div>
-                <div>Rating: {currentProfileData.rating}</div>
             </div>
         </>
     );
