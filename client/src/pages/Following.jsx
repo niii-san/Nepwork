a3d95cf5e9520612452c4e3ac81f89e2bc1cd721
Nishan Bista <nishanbista525@gmail.com>
2025-02-01 23:14:47 +0545

    feat: followers list

--- client/src/pages/Following.jsx
+++ client/src/pages/Following.jsx
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 import { useParams } from "react-router";
 import api from "../utils/api";
 import { useAuth, useUser } from "../stores";
-import { Button, ConnectionUserList, Loader } from "../components";
+import { ConnectionUserList, Loader } from "../components";
 
 function Following() {
     const { userId } = useParams();
@@ -37,7 +37,7 @@ function Following() {
 
     return (
         <div className="max-w-2xl mx-auto p-4 min-h-screen">
-            <h1 className="text-2xl font-bold text-gray-800 mb-6">Following</h1>
+            <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">Following</h1>
             <div className="space-y-4">
                 {followingList.map((item) => (
                     <ConnectionUserList
